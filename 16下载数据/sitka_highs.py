39405113a72662a34fdee3f870dd3a8c3983f25f
baiyc <1197180468@qq.conm>
2022-06-27 22:26:10 +0800

    change

--- 16下载数据/sitka_highs.py
+++ 16下载数据/sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -21,7 +21,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 # 设置图标标题并给坐标轴加上标签
-ax.set_title('2018年7月每日最高温度', fontsize=24)
+ax.set_title('2018年每日最高温度', fontsize=24)
 ax.set_xlabel('', fontsize=14)
 ax.set_ylabel('温度', fontsize=14)
 ax.tick_params(axis='both', which='major')
